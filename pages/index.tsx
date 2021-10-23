ef6e43493211902929e323c155e5c66c0a10c1da
Gefferon Batista <geffbatista@gmail.com>
2021-10-23 14:02:43 -0300

    The first of many

---
+++ pages/index.tsx
@@ -0,0 +1,30 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-900">
+      <Head>
+        <title>Gefferson Batista - Front End Developer </title>
+        <meta
+          name="description"
+          content="Gefferson Batista - Front End Developer"
+        />
+        <link rel="icon" href="/favicon.ico" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <main className="flex flex-col items-left justify-center w-full flex-1 px-8 text-left text-gray-300">
+        <h1 className="text-6xl font-bold">Gefferson Batista </h1>
+        <h2 className="text-2x1 font-bold">Front End Developer</h2>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
